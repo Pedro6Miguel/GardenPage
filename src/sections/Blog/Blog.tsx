21451d41db1e6adade93d215f74818717678e1a3
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2023-02-20 03:20:03 +0000

    Blog section update

--- src/sections/Blog/Blog.tsx
+++ src/sections/Blog/Blog.tsx
@@ -2,15 +2,35 @@ import React from 'react'
 import { BLOG_DATA, BlogData } from './BlogData';
 import { useNavigate } from "react-router-dom";
 
+import Arrow from '../../../public/images/Arrow.png'
+import DateIcon from '../../../public/images/DateIcon.png'
+
 export const Blog = () => {
     const navigate = useNavigate();
-  return (
-    <div >
-        {BLOG_DATA.map((data, i) =>(
+  
+    return (
+    <div className='my-[110px] mx-[220px] max-[1470px]:mx-[120px] max-[1100px]:mx-[80px] max-[1100px]:my-[70px]'>
+        {/*{BLOG_DATA.map((data, i) =>(
             <div onClick={() => {navigate(`/${data.id}`);}} key={data.id} className='flex flex-col items-center gap-[10px] cursor-pointer'>
-                <p className='text-text font-normal text-center text-[34px]'>{data.name}</p>
+                <p className='text-text font-normal text-center text-[34px]'>{data.title}</p>
             </div>
-        ))}
+        ))}*/}
+        <h2 className='text-green capitalize text-center'>Interesting blog to read</h2>
+        <div>
+            <img src="https://media.discordapp.net/attachments/905837522813337613/1077060272541405355/Post3.png" alt="" />
+            <h4>The benefits of plants in your room</h4>
+            <h5>Plants in your room can bring numerous benefits, such as improved air quality, reduced stress, and increased feelings of well-being....</h5>
+            <div className='flex justify-between'>
+                <div className='flex items-center gap-2'>
+                    <img className='w-[18px] h-[18px]' src={DateIcon} alt="" />
+                    <p className='text-[#121212a1] text-base'>January 20, 2023</p>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <p className='text-base text-green font-bold'>Read more</p>
+                    <img className='w-[16px] h-[12px]' src={Arrow} alt="" />
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
--- src/sections/Blog/BlogData.tsx
+++ src/sections/Blog/BlogData.tsx
@@ -1,68 +1,31 @@
 export interface BlogData {
   id: number;
   image: string;
-  name: string;
+  title: string;
+  description: string;
+  data: string;
 }
 
   export const BLOG_DATA: BlogData[] = [
       {
           id: 0,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849089430409327/1.png",
-          name: "Diablo® II ressurected"
+          image: "https://media.discordapp.net/attachments/905837522813337613/1077060272751132742/Post1.png",
+          title: "More productive with an atmosphere of greenery",
+          description: "An atmosphere of greenery can increase productivity in the workplace. Studies show that plants improve air quality and decrease stress...",
+          data: "January 10, 2023"
       },
       {
           id: 1,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849180509712466/2.png",
-          name: "Overwatch® 2"
+          image: "https://media.discordapp.net/attachments/905837522813337613/1077060273006972938/Post2.png",
+          title: "The benefits of plants in your room",
+          description: "Plants in your room can bring numerous benefits, such as improved air quality, reduced stress, and increased feelings of well-being...",
+          data: "January 20, 2023"
       },
       {
           id: 2,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849180903981137/3.png",
-          name: "World of Warcraft®"
+          image: "https://media.discordapp.net/attachments/905837522813337613/1077060272541405355/Post3.png",
+          title: "Hobbyist plants in the house",
+          description: "Having hobbyist plants in the house is a great way to bring nature indoors. Not only do they purify the air, but they....",
+          data: "January 28, 2023"
       },
-      {
-          id: 3,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849181633789952/4.png",
-          name: "Hearthstone®"
-      },
-      {
-          id: 4,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849181273083924/5.png",
-          name: "Heroes of the storm®"
-      },
-      {
-          id: 5,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849182070001754/6.png",
-          name: "Warcraft® III Reforged"
-      },
-      {
-          id: 6,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849182409732136/7.png",
-          name: "Diablo® IV"
-      },
-      {
-          id: 7,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849182745280582/8.png",
-          name: "Diablo® Immortal"
-      },
-      {
-          id: 8,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849183059857458/9.png",
-          name: "Diablo® III"
-      },
-      {
-          id: 9,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849180190953503/10.png",
-          name: "StarCraft® II"
-      },
-      {
-          id: 10,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849275368095844/11.png",
-          name: "StarCraft® Remastered"
-      },
-      {
-          id: 11,
-          image: "https://media.discordapp.net/attachments/905837522813337613/1049849275653292112/12.png",
-          name: "Arcade da Blizzard®"
-      },  
   ];
