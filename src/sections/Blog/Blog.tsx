e82f1dc26d40642260260a07470d88f1ae682d85
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2023-02-23 00:30:36 +0000

    Blog section responsiveness

--- src/sections/Blog/Blog.tsx
+++ src/sections/Blog/Blog.tsx
@@ -20,13 +20,13 @@ export const Blog = () => {
             </div>
         ))}*/}
         <h2 className='text-green capitalize text-center pb-10'>Interesting blog to read</h2>
-        <div className='flex gap-16 max-[1120px]:gap-12 max-[1120px]'>
+        <div className='flex gap-16 max-[1120px]:gap-12 max-[1285px]:flex-col'>
             {BLOG_DATA.map((data, i) =>(
             <div onClick={() => {navigate(`/${data.id}`); scroll()}} className='border-2 flex flex-col justify-between' key={data.id}>
                 <img className='bg-green rounded-[10px] h-[300px] object-cover max-[1280px]:h-[240px]' src={data.image} alt="" />
                 <h4 className='text-green pt-2 pb-2'>{data.title}</h4>
                 <h5 className='text-dark pb-4'>{data.description}</h5>
-                <div className='flex justify-between'>
+                <div className='flex justify-between gap-4'>
                     <div className='flex items-center gap-2'>
                         <img className='w-[18px] h-[18px]' src={DateIcon} alt="" />
                         <p className='text-[#121212a1] text-base'>{data.date}</p>
