3f5450ef7ac2b22171f42e1a4bc1ffabe32394e3
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2023-02-27 18:07:00 +0000

    Responsive nav updatee

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Logo from '../../../public/images/Logo.png'
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(true)
+
   return (
-    <nav className='bg-dark opacity-20 pt-8 w-[100vw] h-[100px] absolute top-0 z-10 flex justify-between items-center px-[220px] max-[1470px]:px-[120px] max-[1100px]:px-[80px]'>
+    <div className={open ? 'overflow-hidden' : 'border-1'}>
+    <nav className='bg-dark pt-8 w-[100%] h-[100px] absolute top-0 z-10 flex justify-between items-center px-[220px] max-[1470px]:px-[120px] max-[1100px]:px-[80px]'>
       <img className='h-[64px]' src={Logo} alt="" />
-      <ul className='flex gap-8 items-center'>
+      <ul className='flex gap-8 items-center max-[880px]:hidden'>
         <li className='text-[18px] text-white'>About</li>
         <li className='text-[18px] text-white'>Planters</li>
         <li className='text-[18px] text-white'>Contact</li>
         <li className='text-[18px] text-white border-2 px-12 py-2 rounded-[4px]'>Call Us</li>
       </ul>
+      <label className='min-[880px]:hidden' htmlFor="check">
+        <input onClick={() => {setOpen(!open)}}  type="checkbox" id="check"/> 
+        <span></span>
+        <span></span>
+        <span></span>
+    </label>
     </nav>
+    { open ? <div className='bg-green w-[100%] h-[100vh] flex justify-center items-center overflow-hidden'>
+      <ul className='flex gap-8 items-center flex-col'>
+        <li className='text-white text-[40px] font-semibold uppercase'>About</li>
+        <li className='text-white text-[40px] font-semibold uppercase'>Planters</li>
+        <li className='text-white text-[40px] font-semibold uppercase'>Contact</li>
+        <li className='text-white text-[40px] font-semibold uppercase'>Call Us</li>
+      </ul>
+    </div>
+    : ''
+    }
+    </div>
   )
 }
--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -4,8 +4,8 @@ import HeroImg from '../../../public/images/Hero.png'
 
 export const Hero = () => {
   return (
-    <div>
-        <img className='w-[100%] h-[758px] object-cover relative bg-bottom' src={HeroImg} alt="" />
+    <div className='relative'>
+        <img className='w-[100%] h-[758px] object-cover bg-bottom' src={HeroImg} alt="" />
         <div className='absolute top-[350px] left-[50%] translate-x-[-50%] translate-y-[-50%] max-[436px]:top-[380px]'>
             <h1 className='text-center text-white line leading-tight max-[930px]:w-[80vw] max-[580px]:text-[48px] max-[418px]:text-[42px] max-[418px]:w-[90vw]'>Nature's Beauty Delivered to You</h1>
             <p className='text-center text-white pt-4 pb-8 max-[930px]:w-[80vw]'>Nature's beauty is just a click away with our online flower and plant shop. We offer a wide variety of flowers that will bring a touch of nature to your home!</p>
