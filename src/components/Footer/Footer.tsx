cb1bb55a491ef3b6df7a0137aa01624313f72407
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2023-03-10 22:44:28 +0000

    Footer update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <div className='bg-green flex flex-col items-center py-16 text-white px-[220px] max-[1470px]:px-[120px] max-[1100px]:px-[80px]'>
       <h3 className='pb-4'>Seasoned. Nimble. Remote.</h3>
       <p className='text-[#e5e5e5] w-[60%] max-[960px]:w-[90%] max-[550px]:w-[100%]'>We're a diverse and passionate team that takes the nature in its unique form to bring you the best of it. We stay light on our feet and truly enjoy delivering great work.</p>
-      <div className='h-[1px] w-[100%] my-14 bg-[#153526] mx-[220px] max-[1470px]:mx-[120px] max-[1100px]:mx-[80px]'></div>
+      <div className='h-[1px] w-[100%] my-14 bg-[#1f4a36] mx-[220px] max-[1470px]:mx-[120px] max-[1100px]:mx-[80px]'></div>
       <div className='flex justify-between w-[100%] flex-wrap gap-5'>
         <div>
           <p className='font-semibold pb-2'>Navigation</p>
